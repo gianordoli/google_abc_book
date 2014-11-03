75b8d15ebcc975cff7b7b97e97d5f8769b548c60
gianordoli <gianordoligabriel@yahoo.com.br>
2014-11-02 23:23:49 -0500

    updated server setup

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 var express = require('express');
 var app     = express();
-var server = app.listen(9001, function(){
-	console.log('Listening on 9001');
+
+app.use(function(req, res, next) {
+	var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+	console.log('incoming request from ---> ' + ip);
+	var url = req.originalUrl;
+	console.log('### requesting ---> ' + url);
+	next();
 });
-var io		= require('socket.io').listen(server);
 
 app.use('/', express.static(__dirname + '/public'));
+
+var server = app.listen(3011, function(){
+	console.log('Listening on 3011');
+});
+var io		= require('socket.io').listen(server);
 
 io.on('connection', function(socket){
   
