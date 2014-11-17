33c5cdc5d76d3614bc22d34a6c3e27ee8f252d33
gianordoli <gianordoligabriel@yahoo.com.br>
2014-11-17 15:23:54 -0500

    Changed script to call multiple pages at once
    - making just one call from the client, with all strings
    - creating multiple pages at once on the server side
    - returning all results in one array

--- app.js
+++ app.js
@@ -24,56 +24,130 @@ app.use('/', express.static(__dirname + '/public'));
 
 /*------------------ SCRAPING --------------------*/
 
+var results = {};
+var letters = [];
+for(var i = 65; i < 91; i++){
+	var value = String.fromCharCode(i);
+	letters.push(value);
+}
+var i = 0;
+
 io.on('connection', function(socket){
   
 	console.log('User connected');
 
-	socket.on('search', function(obj){
+	socket.on('search', function(data){
 		
-		var query = obj.v;
-		var index = obj.i;
-
+		console.log(data);
 		console.log('Called search.');
-		
+
 		phantom.create(function(ph) {
 			
 			console.log('Created Phantom object.');
 
-			return ph.createPage(function(page) {
+			var query = data[letters[i]];
+			console.log(query);				
 
-				console.log('Created page.');
-			    
-			    return page.open("https://www.google.com/search?site=imghp&tbm=isch&q="+query, function(status) {
-			      
-			      console.log("opened site? ", status);
+			return ph.createPage(function(page){
+				newPage(ph, page, query);
+			});
 
-		            setTimeout(function() {
+		});		
+		
 
-		                return page.evaluate(function() {
+		function newPage(ph, page, query) {
 
-							var images = document.getElementsByTagName('img');
+			console.log('Created new page.');
+		    
+		    return page.open("https://www.google.com/search?site=imghp&tbm=isch&q=" + query, function(status) {
+		      
+		      console.log("opened site? ", status);
 
-							return images[0].src
-
-		                }, function(result) {
-		                    console.log(result);
-		                    console.log(query);
-			        		while(query.indexOf(' ') > -1){
-			        			query = query.replace(' ', '_') 
-			        		}
-		                    var obj = {
-		                    	i: index,
-		                    	name: query,
-		                    	path: result
-		                    }
-		                    socket.emit('write', obj);
-		                    ph.exit();
-		                });
-		            }, 100);
-			 
-			    });
+	            setTimeout(scrape(ph, page, query), 100);
+		 
 		    });
-		});
+	    }
+
+		function scrape(ph, page, query){
+
+			console.log('Started scraping...')
+
+            return page.evaluate(
+
+            	// Scrape
+            	function() {
+					var images = document.getElementsByTagName('img');
+					return images[0].src
+
+            	},
+
+            	// Send back results
+            	function(result) {
+                    console.log('Found: ' + result);
+                    console.log(query);
+	        		while(query.indexOf(' ') > -1){
+	        			query = query.replace(' ', '_') 
+	        		}
+                    results[query] = result;
+                    // console.log(results);
+
+                    if(i < letters.length - 1){
+                    	i++;
+						var newQuery = data[letters[i]];
+						console.log('Calling search for next: ' + newQuery);	                    	
+                    	newPage(ph, page, newQuery)
+                    }else{
+	                    socket.emit('write', results);
+	                    ph.exit();                    	
+                    }
+            	}
+           );	
+		}
+
+		// var query = obj.v;
+		// var index = obj.i;
+
+		// console.log('Called search.');
+		
+		// phantom.create(function(ph) {
+			
+		// 	console.log('Created Phantom object.');
+
+		// 	return ph.createPage(function(page) {
+
+		// 		console.log('Created page.');
+			    
+		// 	    return page.open("https://www.google.com/search?site=imghp&tbm=isch&q="+query, function(status) {
+			      
+		// 	      console.log("opened site? ", status);
+
+		//             setTimeout(function() {
+
+		//                 return page.evaluate(function() {
+
+		// 					var images = document.getElementsByTagName('img');
+
+		// 					return images[0].src
+
+		//                 }, function(result) {
+		//                     console.log(result);
+		//                     console.log(query);
+		// 	        		while(query.indexOf(' ') > -1){
+		// 	        			query = query.replace(' ', '_') 
+		// 	        		}
+		//                     var obj = {
+		//                     	i: index,
+		//                     	name: query,
+		//                     	path: result
+		//                     }
+		//                     socket.emit('write', obj);
+		//                     ph.exit();
+		//                 });
+		//             }, 100);
+			 
+		// 	    });
+		//     });
+		// });
 
 	});
 });
